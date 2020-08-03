e60cfbb5ad36f44365be2f5807f712093a4ad08b
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-08-03 21:03:05 +0100

    Playlist data is now updated dynamically in UI

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 A tool for downloading and exploring YouTube's auto-captions for playlists/channels etc.
 
 It's still very rough, everything runs as a dev build/server, and only surface level optimisation/architecture
-is present. It's still enough to function though, and can handle (a little sluggishly) 2000+ video playlists 
+is present. It's still enough to function though, and can handle (a little sluggishly) 2000+ video playlists
 with at least a couple months of dialogue.
 
 ## How does it work?
@@ -28,13 +28,14 @@ If you've ever wanted to quickly search for something that was said in a series 
 2. Clone the repo
 3. `npm i` - inside the repo to install dependencies
 4. `npm run ytdl [playlist_url] [playlist_url] ...` - Downloads playlists to be explored, wait for it to finish before moving on
-   1.  Additionally specify `update` with `npm run ytdl` to update any existing playlist data, in addition to any new URLs provided
-   2.  Additionally specify `threads=x` with `npm run ytdl` to set the number of youtube-dl processes to spawn [min=1, max=100]
+   1. Additionally specify `update` with `npm run ytdl` to update any existing playlist data, in addition to any new URLs provided
+   2. Additionally specify `threads=x` with `npm run ytdl` to set the number of youtube-dl processes to spawn [min=1, max=100]
 5. `npm start` - Should open the UI in web browser!
 6. Spam `ctrl-c` to close I guess
 
 # Caveats - these are important
-1. You *must* provide a playlist. Even if you just want one video, wrap it in a playlist! 
+
+1. You _must_ provide a playlist. Even if you just want one video, wrap it in a playlist!
    1. YouTube does a lot with playlists. For example, clicking "Play all" on a channel uploads will give you a playlist link. This is how you index an entire channel.
 2. The subtitles are, in most cases, provided by YouTube's auto-captions, though it does download official ones if present!
 3. This tool does NO speech-to-text. All captions are from YouTube, so if the subtitles say one thing, but the video says another, that's not my fault
@@ -45,12 +46,12 @@ If you've ever wanted to quickly search for something that was said in a series 
 ## Some side-effects of over-using the tool
 
 #### Potential rate limiting
+
 <img src="https://i.vgy.me/upSb0O.png" width="450">
 
 #### YouTube blocking connections outside of browser
+
 <img src="https://i.vgy.me/aRqiSR.png" width="450">
-
-
 
 # To-Do:
 
@@ -59,11 +60,12 @@ If you've ever wanted to quickly search for something that was said in a series 
 1. ~Pull playlist information~
 1. ~Pull video information~
 1. ~Pull subtitles for each video~
-1. Secondary STT method for extracting subtitles
 1. ~Implement basic API for front-end to access the data~
+1. ~Secondary STT method for extracting subtitles~ - I don't want to be liable for mis-subtitling.
 1. ~Figure out how to efficiently search for text across millions of subtitle entries~ [FlexSearch](https://github.com/nextapps-de/flexsearch)
-1. Include playlist title in Ytdl
-1. Refresh playlist_data folder on changes so you don't need to keep restarting the UI
+1. ~Refresh playlist_data folder on changes so you don't need to keep restarting the UI~
+1. Include playlist title in Ytd
+1. Download playlists from UI
 
 ### UI
 
@@ -77,11 +79,13 @@ If you've ever wanted to quickly search for something that was said in a series 
 # Images
 
 ### Playlist Page
+
 <img src="https://i.vgy.me/Y3vEDW.png" width="600">
 
 #### Exploring a Playlist's Subtitles
+
 <img src="https://i.vgy.me/wiXggL.png" width="600">
 
 #### Jumping Right to the Timestamp
-<img src="https://i.vgy.me/S9MmxB.png" width="600">
 
+<img src="https://i.vgy.me/S9MmxB.png" width="600">
--- server.ts
+++ server.ts
@@ -1,42 +1,103 @@
 import express from "express";
-import path from "path";
 import { PlaylistJSON } from "./ytdl";
 import * as fs from "fs";
 import humanizeDuration from "humanize-duration";
 import cors from "cors";
+import md5 from "object-hash";
 const app = express();
 const port = 8080; // default port to listen
 const playlistDirectory = "./playlist_data";
 const playlists = new Map<string, PlaylistJSON>();
+const playlistHashes = new Map<string, string>();
 const playlistMetadata = new Map<string, PlaylistMetadata>();
 
 app.use(cors());
 
+const generateMetadata = async (playlistData: PlaylistJSON) => {
+  if (Object.keys(playlistData.videos).length === 0) return false;
+  let totalLength = 0;
+  Object.values(playlistData.videos).forEach((v) => {
+    if (v.duration) totalLength += v.duration;
+  });
+  const metadata: PlaylistMetadata = {
+    id: playlistData.id,
+    videoCount: Object.keys(playlistData.videos).length,
+    videosWithSubs: Object.keys(playlistData.subs).filter(
+      (s) => playlistData.subs[s].length !== 0
+    ).length,
+    thumbnail: Object.values(playlistData.videos)[0].thumbnail,
+    totalLength: humanizeDuration(1000 * totalLength, { largest: 2 }),
+  };
+
+  playlistMetadata.set(playlistData.id, metadata);
+
+  return true;
+};
+
 const initPlaylistData = async () => {
+  fs.mkdirSync(playlistDirectory, { recursive: true });
   const dir: string[] = fs.readdirSync(playlistDirectory);
+  const metadataPromise = [];
   for (const filename of dir) {
     const playlistData: PlaylistJSON = await JSON.parse(
       fs.readFileSync(playlistDirectory + "/" + filename, "utf-8")
     );
-
-    if (Object.keys(playlistData.videos).length === 0) continue;
-    let totalLength = 0;
-    Object.values(playlistData.videos).forEach((v) => {
-      if (v.duration) totalLength += v.duration;
-    });
-    const metadata: PlaylistMetadata = {
-      id: playlistData.id,
-      videoCount: Object.keys(playlistData.videos).length,
-      videosWithSubs: Object.keys(playlistData.subs).filter(
-        (s) => playlistData.subs[s].length !== 0
-      ).length,
-      thumbnail: Object.values(playlistData.videos)[0].thumbnail,
-      totalLength: humanizeDuration(1000 * totalLength, { largest: 2 }),
-    };
-
-    playlistMetadata.set(playlistData.id, metadata);
+    metadataPromise.push(generateMetadata(playlistData));
+    playlistHashes.set(filename, md5(playlistData.videos));
     playlists.set(filename.split(".json")[0], playlistData);
   }
+  await Promise.all(metadataPromise);
+};
+
+const checkUpdates = async (id?: string) => {
+  let toUpdate: PlaylistJSON[] = [];
+  let data;
+  if (id) {
+    const filename = id + ".json";
+    try {
+      data = await JSON.parse(
+        fs.readFileSync(playlistDirectory + "/" + filename, "utf-8")
+      );
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+
+    if (md5(data.videos) !== playlistHashes.get(filename)) {
+      playlists.set(filename, data);
+      toUpdate.push(data);
+    }
+  } else {
+    const playlistDir = fs.readdirSync(playlistDirectory);
+    const readAsync = playlistDir.map((f) =>
+      JSON.parse(fs.readFileSync(playlistDirectory + "/" + f, "utf-8"))
+    );
+    await Promise.all(readAsync);
+    for (const file of playlistDir) {
+      let data = readAsync.find((d) => file === d.id + ".json");
+
+      if (md5(data.videos) !== playlistHashes.get(file)) {
+        playlists.set(file, data);
+        toUpdate.push(data);
+      }
+    }
+    for (const playlist of playlists.keys()) {
+      if (!playlistDir.includes(playlist + ".json")) {
+        playlists.delete(playlist);
+        playlistMetadata.delete(playlist);
+        playlistHashes.delete(playlist);
+      }
+    }
+  }
+
+  if (toUpdate.length !== 0) {
+    const metadataPromise: Promise<boolean>[] = [];
+    toUpdate.forEach((data) => {
+      metadataPromise.push(generateMetadata(data));
+    });
+    await Promise.all(metadataPromise);
+  }
+  return true;
 };
 
 export interface PlaylistMetadata {
@@ -49,16 +110,23 @@ export interface PlaylistMetadata {
 
 const initRoutes = async () => {
   app.get("/playlist_metadata", (req, res) => {
-    res.json([...playlistMetadata.values()]);
+    checkUpdates()
+      .then(() => res.json([...playlistMetadata.values()]))
+      .catch((e) => res.status(500).send(e));
   });
 
   app.get("/playlist/:id", (req, res) => {
     const { id } = req.params;
-
-    if (!playlists.has(id)) res.status(404).send();
+    if (!playlists.has(id)) {
+      res.status(404).send();
+      return;
+    }
     const playlist = playlists.get(id);
 
-    if (!playlist) res.status(500).send();
+    if (!playlist) {
+      res.status(500).send();
+      return;
+    }
     res.json(playlist);
   });
 };
@@ -67,7 +135,6 @@ initPlaylistData()
   .then(initRoutes)
   .then(() => {
     app.listen(port, () => {
-      // tslint:disable-next-line:no-console
       console.log(`server started at http://localhost:${port}`);
     });
   });
